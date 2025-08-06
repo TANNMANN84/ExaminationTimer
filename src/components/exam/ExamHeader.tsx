fd5f8710323f718be55cbf06367024787c35e241
TANNMANN84 <144026524+TANNMANN84@users.noreply.github.com>
2025-08-06 15:16:56 +1000

    Update ExamHeader.tsx

--- src/components/exam/ExamHeader.tsx
+++ src/components/exam/ExamHeader.tsx
@@ -6,7 +6,7 @@ import FontControl from './FontControl';
 const ExamHeader: React.FC = () => {
     const { state } = useAppContext();
     const { settings, sessionMode } = state;
-    // Get the new separated date strings from our updated hook
+    // Correctly destructure the new variables from the hook
     const { timeValue, timePeriod, dayString, dateString } = useTimer();
     const isStandardised = sessionMode === 'standardised';
 
@@ -25,7 +25,7 @@ const ExamHeader: React.FC = () => {
                     </div>
                 )}
 
-                {/* --- Column 2: Title & School Info (Now on separate lines) --- */}
+                {/* --- Column 2: Title & School Info --- */}
                 <div className="flex flex-col items-start text-left">
                     <div className="flex items-center space-x-2">
                         <FontControl elementId="header-session-title" direction="down" />
@@ -34,7 +34,6 @@ const ExamHeader: React.FC = () => {
                         </h1>
                         <FontControl elementId="header-session-title" direction="up" />
                     </div>
-                    {/* School Name */}
                     {settings.showSchool && (
                          <div className="flex items-center space-x-2 mt-1">
                             <FontControl elementId="header-school-info" direction="down" />
@@ -44,7 +43,6 @@ const ExamHeader: React.FC = () => {
                             <FontControl elementId="header-school-info" direction="up" />
                         </div>
                     )}
-                    {/* Centre Number (only shows if school name is also shown) */}
                     {settings.showSchool && !isStandardised && settings.showCentre && (
                         <p className="text-xl text-slate-500 dark:text-slate-400 mt-1 pl-8">
                            Centre: {settings.centreNumber}
@@ -68,7 +66,7 @@ const ExamHeader: React.FC = () => {
                     <FontControl elementId="header-time" direction="up" />
                 </div>
 
-                {/* --- Column 4: Date (Now multi-line and centered) --- */}
+                {/* --- Column 4: Date --- */}
                 <div className="flex flex-col items-center text-center">
                      <div className="flex items-center space-x-2">
                         <FontControl elementId="header-date" direction="down" />
